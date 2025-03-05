9fc6b834e83ecfe179db36d3a119f9053cc45b42
Lucie123456789 <152063019+Lucie123456789@users.noreply.github.com>
2025-03-05 10:35:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ def main():
     try:
         automate = Automate.lire_fichier(nom_fichier)
         automate.afficher()
-        print("L'automate est-il déterministe ?", automate.est_deterministe())
-        print("L'automate est-il complet ?", automate.est_complet())
-        print("L'automate est-il standard ?", automate.est_standard())
+        print("Déterministe ?", automate.est_deterministe())
+        print("Complet ?", automate.est_complet())
+        print("Standard ?", automate.est_standard())
+        
+        if automate.est_deterministe() and automate.est_complet():
+            partitions = automate.minimisation()
+            print("Résultat de la minimisation:")
+            print(partitions)
+        else:
+            print("L'automate doit être déterministe et complet avant minimisation.")
+        
     except FileNotFoundError:
         print(f"Le fichier {nom_fichier} n'a pas été trouvé.")
 
