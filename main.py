5780e12a5f1f1804f74854ffb7a5d2c927eefd63
Lucie123456789 <152063019+Lucie123456789@users.noreply.github.com>
2025-03-05 10:32:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ def main():
     try:
         automate = Automate.lire_fichier(nom_fichier)
         automate.afficher()
+        print("L'automate est-il déterministe ?", automate.est_deterministe())
+        print("L'automate est-il complet ?", automate.est_complet())
+        print("L'automate est-il standard ?", automate.est_standard())
     except FileNotFoundError:
-        print(f"Erreur : Le fichier {nom_fichier} n'a pas été trouvé.")
+        print(f"Le fichier {nom_fichier} n'a pas été trouvé.")
 
 if __name__ == "__main__":
     main()
